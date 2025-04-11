b80acae86c53827682b821f054f37a436311a981
Jennykings <jennykings51@gmail.com>
2025-04-11 09:02:40 -0500

    update use case

---
+++ src/CustomerManagement/Customer/application/DeleteCustomerUseCase.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\CustomerManagement\Customer\Application;
+
+use Src\CustomerManagement\Customer\Domain\Contracts\CustomerRepositoryContract;
+
+final class DeleteCustomerUseCase
+{
+	private $repository;
+
+	public function __construct(CustomerRepositoryContract $repository)
+	{
+		$this -> repository = $repository;
+	}
+
+	public function __invoke()
+	{
+		//
+	}
+}
